117c5969b46e0c56d1a6315644b613b178614152
eityans <eityans@gmail.com>
2019-08-04 18:57:43 +0900

    リンク先を取得

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import urllib
 import configparser
+import requests
+from bs4 import BeautifulSoup
+
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 url = config.get('general', 'url')
+user_agent = config.get('general', 'user_agent')
 
-print(url)
+headers = {"User-Agent": user_agent}
+r = requests.get(url, timeout=1, headers=headers)
+soup = BeautifulSoup(r.content, "html5lib")
+
+print(soup)
